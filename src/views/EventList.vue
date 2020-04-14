9886895fde2fbeb581713b37f3e86b73a7dbcc1a
Dao Tien Nam <43399562+daotiennamhl@users.noreply.github.com>
2020-04-14 15:33:41 +0700

    dynamic routing & history mode

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Even list</h1>
+  <div>
+    <h1>Even list</h1>
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
+      >Show Event #1</router-link>
+  </div>
 </template>
