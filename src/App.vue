923dd680e81988d95dad57471f46206887442293
Dao Tien Nam <43399562+daotiennamhl@users.noreply.github.com>
2020-04-18 07:19:15 +0700

    #9 API calls with Axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <NavBar/>
+    <NavBar />
     <router-view />
   </div>
 </template>
@@ -12,7 +12,7 @@ export default {
   components: {
     NavBar
   }
-}
+};
 </script>
 
 <style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
     <h1>Even list</h1>
-    <EventCard/>
+    <div v-for="event in events" :key="event.id">
+      <EventCard :event="event" />
+    </div>
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard";
+import EventService from "@/services/EventService.js";
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
 </script>
