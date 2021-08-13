2a1b6d608d6c6a3fd127da70d4a1a35dd5d1b67c
DESKTOP-40THMNO\daotiennam <daotiennamhl120699@gmail.com>
2021-08-13 22:09:32 +0700

    git puuupdate

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Web3 from 'web3';
+
+const web3 = new Web3(Web3.givenProvider || process.env.VUE_APP_WEB3_FALLBACK_PROVIDER);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  provide: {
+    web3,
+  },
   render: h => h(App)
 }).$mount("#app");
